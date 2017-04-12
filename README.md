dce0976915fc8620a69ca5281a446f87aa4cfdbe
root <root@cpanel.ids.ninja>
2017-04-12 15:00:12 +0000

    commit message

---
+++ README.md
@@ -0,0 +1,63 @@
+# botbuster.py
+Automated banning of twitch chat bots based on account creation date. I made this because a friend of mine was afraid to stream because of being attacked by chat bots. Hopefully this helps others who get attacked too. DM me on twitter @IDSninja or stop by my twitch channel for help https://twitch.tv/daytona_675
+
+I'm willing to run the bot myself in your channel if you need help. It would help me improve the protective measures.
+
+
+#Installation
+
+Requires python. I know it will work with 3.5 and 3.4. I think some of the dictionary handling is incompatible with 2.7.
+Tested on Windows, should work on *NIX environments too.
+
+https://www.python.org/downloads/
+
+Modules used: socket, time, re, requests, json
+
+If some of these don't come standard in your python installation, they can be installed by opening cmd or terminal and typing:
+
+```pip install requests json```
+
+Download the botbuster.py and open with a text editor to configure authentication and channel.
+
+#Configuration
+
+###chat_user 
+Set this value to the twitch user name which your bot will use. You CAN use your channel user, but the bot will send messages as you.
+This user needs to be a mod in the channel which it will be protecting.
+
+###chat_pass
+This is the oauth password for the above user. To obtain this, visist the following website while logged in as the above user:
+http://www.twitchapps.com/tmi/
+
+###chat_chan
+This is the name of your channel on twitch that the bot will be joining. For example, mine is daytona_675.
+
+###punishment
+This value sets if the bot bans or times users out. The value 'ban' will cause permanent bans, any other value will cause timeouts. 
+
+#Usage
+
+run botbuster.py
+
+In your twitch channel you should see your bot user say 'Reporting for duty!'
+
+###Commands
+All commands are useable by channel moderators.
+
+#####!blacklist username
+This will automatically look up the creation date of a user and blacklist it. This is the recommended method so you don't have to worry about time zones. BTTV uses your local time zone and my bot just uses the GMT value twitch provides.
+
+#####!startbans
+This command will start the ban loops. It loops through each user in the room checking their creation date. It DOES make an http request to twitch's kraken API for EVERY user unless they have been banned or whitelisted. I will probably implement a cache dictionary of users already looked up in the near future. After finishing the loop, it pauses for 15 seconds.
+
+#####!stopbans
+This stops banning users and clears the blacklisted date list
+
+#####!whitelist username
+This command will prevent a user from being banned by the bot and also send the unban command for convenience. 
+
+#####!bld YYYY-MM-DD
+Blacklists a date manually. IMPORTANT: This date is in GMT. BTTV shows the date based on your LOCAL time zone.
+
+#####!unlist YYYY-MM-DD
+Manually unlists a blacklisted date
---
+++ botbuster.py
@@ -0,0 +1,358 @@
+#! python3
+# -*- coding: utf-8 -*-
+# Author: Leo675
+
+import socket
+import time
+import re
+import requests
+import json
+import sqlite3
+from threading import Thread
+
+
+# Function for threaded asynchronous functions decorator @async
+def async(func):
+    from functools import wraps
+    
+    @wraps(func)
+    def async_func(*args, **kwargs):
+        f = Thread(target = func, args = args, kwargs = kwargs)
+        f.start()
+        return
+    return async_func
+
+class BotBuster():
+    # The twitch user which will be acting as your bot
+    chat_user = 'daybot_675'
+
+    # The oauth password for the twitch user which will be operating
+    # Obtain this value by visiting this page while logged in as your bot user: http://www.twitchapps.com/tmi/
+    # example: 
+    chat_pass = 'oauth:'
+
+    # The channel the bot will be operating in (your channel's name)
+    chat_chan = 'daytona_675'
+    
+    # Client ID for api requests, this is the general ID of the web client
+    client_id = "jzkbprff40iqj646a697cyrvl0zt2m6"
+    
+    # Punishment method; change to timeout (or any word other than ban) to make it timeout instead of ban
+    punishment = 'timeout'
+
+    # If using timeout punishment, duration of the timeout
+    timeout_duration = 3600
+
+    # Twitch IRC server info: the host and port should not need to be changed
+    chat_host = "irc.chat.twitch.tv"
+    chat_port = 6667
+
+    # Persistent database file. Makes file name based on chat channel. Only change to make a combined database for multiple channels.
+    database_name = '{}_database.sqlite'.format(chat_chan)
+    
+    banned_users = []
+    whitelisted_users = []
+    banned_dates = []
+    
+    #Maps indexes to names, QoL, don't touch unless changing data structure of database
+    user_name = 0
+    creation_date = 1
+    user_status = 2 # None = not banned, 1 = timed out, 2 = banned, 3 = whitelisted
+    time_stamp = 3
+    last_message = int(time.time())
+    
+    # List of commands which use regular expressions. Only change the left side and make sure to leave the ^
+    commands = {
+        '^!startbans': 'start_banning',
+        '^!blacklist': 'blacklist_user',
+        '^!bld': 'blacklist_date',
+        '^!unlist' : 'unlist_date',
+        '^!whitelist': 'whitelist_user',
+        '^!stopbans': 'stop_banning',
+        '^!wlshow': 'show_whitelist',
+        }
+
+    mitigation_active = 0
+    
+    def __init__(self):
+        return
+    
+    def init_database(self):
+        try:
+            self.load_database()
+            print('loading db')
+        except:
+            print('creating db')
+            # Maps indexes to names
+            self.user_name = 0
+            self.creation_date = 1
+            self.user_status = 2 # None = not banned, 1 = timed out, 2 = banned, 3 = whitelisted
+            self.time_stamp = 3
+
+            create_table = 'create table if not exists {} ( {}, {}, {}, {} )'
+            epoch_time_stamp = "time_stamp timestamp default ( strftime('%s', 'now') )"
+
+            create_user_table = create_table.format('user_table', 'user_name id text primary key', 'creation_date text', 'user_status integer', epoch_time_stamp)
+
+            conn = sqlite3.connect(self.database_name)
+            c = conn.cursor()
+            c.execute(create_user_table)
+
+            # insert_row = 'insert into {} ({}) values (?)'
+            # insert_user = insert_row.format('user_table', 'user_name')
+            # c.execute(insert_user, ('test_user',))
+            # c.execute("select * from 'user_table'")
+            conn.commit()
+            conn.close()
+
+    # Gets list of chatters and updates admin list
+    def get_chatters(self):
+        # Loop ends when a value is returned
+        while 1:
+            self.chatter_list = ()
+            admins = ()
+            # Uses try because sometimes the connection times out
+            try:
+                r = requests.get('https://tmi.twitch.tv/group/user/{}/chatters'.format(self.chat_chan))
+            except:
+                time.sleep(1)
+                continue
+            
+            if r.status_code == 200:
+                r = json.loads(r.text)
+                # Builds the list of chatters
+                for chatter in r['chatters']['viewers']:
+                    self.chatter_list += (chatter,)
+                
+                # Dynamic moderators list, kinda hackish but if you unmod someone they will be removed on the next loop
+                for moderator in r['chatters']['moderators']:
+                    admins += (moderator,)
+                
+                self.admin_list = admins
+                return self.chatter_list
+            time.sleep(3)
+            
+    def load_database(self):
+        self.buster_database = {}
+        conn = sqlite3.connect(self.database_name)
+        c = conn.cursor()
+        select_all = 'select * from user_table;'
+        c.execute(select_all)
+        rows = c.fetchall()
+        print(rows)
+        for row in rows:
+            self.buster_database.update({row[self.user_name] : [row[self.creation_date], row[self.user_status], row[self.time_stamp]]})
+            if row[self.user_status] == 'whitelisted':
+                print('adding {} to whitelist'.format(row[self.user_name]))
+                self.whitelisted_users.append(row[self.user_name])
+            if row[self.user_status] == 'blacklisted':
+                if row[self.creation_date] not in self.banned_dates:
+                    self.banned_dates.append(row[self.creation_date])
+        conn.close()
+        return self.buster_database
+
+    def update_db(self, user, status):
+        ctime = int(time.time())
+        print('checking date created')
+        date_created = get_creation_date(user)
+        print('date check finished')
+        self.buster_database.update({user : [date_created, status, ctime]})
+        sql_query = "update user_table set user_name = '{}', creation_date = '{}', user_status = '{}', time_stamp = '{}';".format(user, date_created, status, ctime)
+        print('executing query: {}'.format(sql_query))
+        conn = sqlite3.connect(self.database_name)
+        c = conn.cursor()
+        c.execute(sql_query)
+        conn.commit()
+        conn.close()
+    
+    
+    # Thread for watching and banning chatters
+    @async
+    def watch_chatters(self):
+        while 1:
+            # Gets the list of chatters in the room, needs to run while mitigation is off
+            self.chatter_list = self.get_chatters()
+            if self.mitigation_active:
+                for chatter in self.chatter_list:
+                    if chatter in self.banned_users:
+                        continue
+                    
+                    if chatter in self.whitelisted_users:
+                        continue
+                        
+                    if get_creation_date(chatter) in self.banned_dates:
+                        if self.punishment == 'ban':
+                            ban(self.s, chatter)
+                            self.banned_users.append(chatter)
+                        else:
+                            self.timeout(self.s, chatter, 3600)
+
+                        print('Current number of banned users: {!s}'.format(len(self.banned_users)))
+            time.sleep(15)
+
+    # Thread for reading chat and watching for user commands
+    @async
+    def read_chat(self):    
+        # Starts infinite loop listening to the IRC server
+        self.connect_chat()
+        while True:
+            response = self.s.recv(1024).decode("utf-8")
+            if len(response) == 0:
+                self.connect_chat()
+                continue
+            
+            # PONG replies to keep the connection alive
+            if response == "PING :tmi.twitch.tv\r\n":
+                self.s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+                continue
+
+            # Separates user and message.
+            chat_object = re.search(r'^:(\w+)![^:]+:(.*)$', response)
+            if chat_object:
+                self.process_chat(chat_object)
+
+    @async
+    def process_chat(self, chat_object):
+        user = chat_object.group(1)
+        msg = chat_object.group(2)
+        # Processes possible commands from the command dictionary
+        for command,action in self.commands.items():
+            if re.search(command, msg):
+                # Manual blacklisting of dates, uses GMT always
+                if action == 'blacklist_date' and user in self.admin_list:
+                    input = re.search('^'+command+'\s+([\d]{4}-[\d]{2}-[\d]{2})', msg)
+                    if input:
+                        # Adds date to the banned list
+                        self.chat(self.s, 'Blacklisting {}'.format(input.group(1)))
+                        self.banned_dates.append(input.group(1))
+                    else:
+                        print('failed bld')
+                        self.chat(self.s, 'Error: invalid format. Use !bld YYYY-MM-DD')
+                    break
+                
+                # Manual unlisting of a date, can also !stopbans to clear the list 
+                elif action == 'unlist_date' and user in self.admin_list:
+                    input = re.search('^'+command+'\s+([\d]{4}-[\d]{2}-[\d]{2})', msg)
+                    if input:
+                        self.banned_dates.remove(input.group(1))
+                    else:
+                        self.chat(self.s, 'Error: invalid format. !unlist YYYY-MM-DD')
+                    break
+
+                # Automatic lookup of a user's creation date blacklisting it.
+                elif action == 'blacklist_user' and user in self.admin_list:
+                    target = re.search('^'+command+'\s*@?([\w\-]+)', msg)
+                    if target:
+                        # Makes sure the user is actually in the room to prevent erroneous bans.
+                        if target.group(1).lower() in self.chatter_list:
+                            user_date = get_creation_date(target.group(1).lower())
+                            self.chat(self.s, 'Blacklisted: {}'.format(user_date))
+                            self.banned_dates.append(user_date)
+                        else:
+                            self.chat(self.s, 'User {} not found in the room. Check spelling or try again in 15 seconds.'.format(target.group(1)))
+                    break
+
+                # Stops bans and clears blacklist
+                elif action == 'stop_banning' and user in self.admin_list:
+                    self.mitigation_active = 0
+                    self.banned_dates = []
+                    self.chat(self.s, 'Turning off chat mitigation.')
+                    break
+
+                # Starts banning users
+                elif action == 'start_banning' and user in self.admin_list:
+                    self.mitigation_active = 1
+                    self.chat(self.s, 'Turning on chat mitigation ═══█❚')
+                    break
+
+                # Unbans and prevents future banning of the user
+                elif action == 'whitelist_user' and user in self.admin_list:
+                    target = re.search('^'+command+'\s*@?([\w\-]+)', msg)
+                    if target:
+                        #if target.group(1) == 'all':
+                        self.chat(self.s, 'Whitelisting user: {}'.format(target.group(1)))
+                        self.update_db(user, 'whitelisted')
+                        self.whitelisted_users.append(target.group(1).lower())
+                        self.unban(self.s, target.group(1).lower())
+                    break
+                elif action == 'show_whitelist' and user in self.admin_list:
+                    self.chat(self.s, repr(self.whitelisted_users))
+                    break
+            
+    # Initial IRC socket connection and authentication
+    def connect_chat(self):
+        s = socket.socket()
+        s.connect((self.chat_host, self.chat_port))
+        s.send("PASS {}\r\n".format(self.chat_pass).encode("utf-8"))
+        s.send("NICK {}\r\n".format(self.chat_user).encode("utf-8"))
+        s.send("CAP REQ :twitch.tv/membership\r\n".encode("utf-8"))
+        s.send("JOIN #{}\r\n".format(self.chat_chan).encode("utf-8"))
+        self.s = s
+        
+    # Sends messages to chat
+    def chat(self, sock, msg):
+        while int(time.time()) - self.last_message  < 2:
+            time.sleep(1)
+        sock.send(bytes('PRIVMSG #%s :%s\r\n' % (self.chat_chan, msg), 'UTF-8'))
+        self.last_message = int(time.time())
+
+    
+    # Permabans users
+    def ban(self, sock, user):
+        self.chat(sock, "/ban {}".format(user))
+
+
+    # Unbans users
+    def unban(self, sock, user):
+        self.chat(sock, "/unban {}".format(user))
+
+        
+    # Times users out
+    def timeout(self, sock, user, secs):
+        self.chat(sock, "/timeout {} {}".format(user, secs))
+
+        
+    # Punishes chatter with specified method
+    def punish(self, chatter):
+        if self.punishment == 'ban':
+            ban(s, chatter)
+            self.banned_users.append(chatter)
+            print('Banning {}'.format(chatter))
+        else:
+            self.timeout(s, chatter, self.timeout_duration)
+            print('Timing Out {}'.format(chatter))
+
+        
+# Function for returning the user's creation date
+def get_creation_date(user):
+    client_id = 'jzkbprff40iqj646a697cyrvl0zt2m6'
+    headers = { 'Client-ID' : client_id }
+    # Loop ends when a value is returned
+    while 1:
+        # Uses try in case of request timeout
+        try:
+            r = requests.get('https://api.twitch.tv/kraken/users/{}'.format(user), headers = headers)
+        except:
+            time.sleep(1)
+            continue
+
+        if r.status_code == 200:
+            # Captures only YYYY-MM-DD
+            date = re.match(
+                '([\d]{4}-[\d]{2}-[\d]{2})',
+                json.loads(r.text)['created_at']
+            )
+            return date.group(1)
+
+       
+if __name__ == '__main__':
+    bot = BotBuster()
+    bot.init_database()
+    print('database initilized')
+    bot.watch_chatters()
+    print('watching chatters')
+    bot.read_chat()
+    print('reading chat')
+    time.sleep(7)
+    bot.chat(bot.s, 'Reporting for duty!')
+    print('looping')
